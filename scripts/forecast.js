c2613107a03bf2133c6879fcae0f87f8ee22996b
RicardVillalba <ricardsax@gmail.com>
2021-07-28 17:48:23 +0200

    api call to get the the weather information is working

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,15 @@
 const key = "";
 
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
 const getCity = async (city) => {
   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
@@ -11,5 +21,10 @@ const getCity = async (city) => {
 };
 
 getCity("barcelona")
-  .then((data) => console.log(data))
+  .then((data) => {
+    return getWeather(data.Key);
+  })
+  .then((data) => {
+    console.log(data);
+  })
   .catch((err) => console.log(err));
